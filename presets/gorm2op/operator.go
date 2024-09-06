d20e7a605a8eecaaacc95da4f9201e1d2e5d9449
Moises P. Sena <moisespsena@gmail.com>
2024-09-06 08:02:35 -0300

    presets.gorm2op: add custom Finder and Callbacks

--- presets/gorm2op/operator.go
+++ presets/gorm2op/operator.go
@@ -20,6 +20,7 @@ type (
 	Preparer func(db *gorm.DB, mode Mode, obj interface{}, id string, params *presets.SearchParams, ctx *web.EventContext) *gorm.DB
 	Deleter  func(db *gorm.DB, obj interface{}, id string, ctx *web.EventContext) (err error)
 	Creator  func(db *gorm.DB, obj interface{}, ctx *web.EventContext) (err error)
+	Finder   func(db *gorm.DB, obj interface{}, ctx *web.EventContext) (result any, err error)
 )
 
 const (
@@ -49,13 +50,29 @@ func DataOperator(db *gorm.DB) (r *DataOperatorBuilder) {
 	return
 }
 
+type Callback func(state *CallbackState) (err error)
+
 type DataOperatorBuilder struct {
 	db         *gorm.DB
 	preparer   Preparer
 	deleter    Deleter
 	creator    Creator
 	updator    Deleter
-	postCreate []func(db *gorm.DB, obj interface{}, ctx *web.EventContext) (err error)
+	finder     Finder
+	preCreate  []Callback
+	postCreate []Callback
+	preUpdate  []Callback
+	postUpdate []Callback
+}
+
+func (b *DataOperatorBuilder) PreUpdate(f ...Callback) *DataOperatorBuilder {
+	b.preUpdate = append(b.preUpdate, f...)
+	return b
+}
+
+func (b *DataOperatorBuilder) PostUpdate(f ...Callback) *DataOperatorBuilder {
+	b.postUpdate = append(b.postUpdate, f...)
+	return b
 }
 
 func (b *DataOperatorBuilder) Updator() Deleter {
@@ -94,6 +111,15 @@ func (b *DataOperatorBuilder) SetDeleter(deleter Deleter) *DataOperatorBuilder {
 	return b
 }
 
+func (b *DataOperatorBuilder) Finder() Finder {
+	return b.finder
+}
+
+func (b *DataOperatorBuilder) SetFinder(finder Finder) *DataOperatorBuilder {
+	b.finder = finder
+	return b
+}
+
 func (b DataOperatorBuilder) Clone() *DataOperatorBuilder {
 	return &b
 }
@@ -120,8 +146,13 @@ func (b *DataOperatorBuilder) WrapPrepare(f func(old Preparer) Preparer) *DataOp
 	return b
 }
 
-func (b *DataOperatorBuilder) PostCreate(cb func(db *gorm.DB, obj interface{}, ctx *web.EventContext) (err error)) *DataOperatorBuilder {
-	b.postCreate = append(b.postCreate, cb)
+func (b *DataOperatorBuilder) PreCreate(cb ...Callback) *DataOperatorBuilder {
+	b.preCreate = append(b.preCreate, cb...)
+	return b
+}
+
+func (b *DataOperatorBuilder) PostCreate(cb ...Callback) *DataOperatorBuilder {
+	b.postCreate = append(b.postCreate, cb...)
 	return b
 }
 
@@ -204,7 +235,12 @@ func (b *DataOperatorBuilder) Search(obj interface{}, params *presets.SearchPara
 		wh = wh.Order(orderBy)
 	}
 
-	err = wh.Find(obj).Error
+	if b.finder != nil {
+		obj, err = b.finder(wh, obj, ctx)
+	} else {
+		err = wh.Find(obj).Error
+	}
+
 	if err != nil {
 		return
 	}
@@ -236,8 +272,7 @@ func (b *DataOperatorBuilder) primarySluggerWhere(wh *gorm.DB, obj interface{}, 
 func (b *DataOperatorBuilder) Fetch(obj interface{}, id string, ctx *web.EventContext) (err error) {
 	db := b.primarySluggerWhere(b.Prepare(Fetch, obj, id, nil, ctx), obj, id)
 	db = db.First(obj)
-	err = db.Error
-	if err != nil {
+	if err = db.Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
 			return presets.ErrRecordNotFound
 		}
@@ -258,37 +293,51 @@ func (b *DataOperatorBuilder) FetchTitle(obj interface{}, id string, ctx *web.Ev
 }
 
 func (b *DataOperatorBuilder) Save(obj interface{}, id string, ctx *web.EventContext) (err error) {
+	if id == "" {
+		return b.Create(obj, ctx)
+	}
+	return b.Update(obj, id, ctx)
+}
+
+func (b *DataOperatorBuilder) Create(obj interface{}, ctx *web.EventContext) (err error) {
 	return b.tx(func(b *DataOperatorBuilder) (err error) {
-		if id == "" {
-			if b.creator != nil {
-				db := b.dbCopy()
-				if err = b.creator(db, obj, ctx); err != nil {
-					return
-				}
-				for _, f := range b.postCreate {
-					if err = f(db, obj, ctx); err != nil {
-						break
-					}
-				}
-			} else {
-				db := b.Prepare(Create, obj, id, nil, ctx)
-				if err = db.Create(obj).Error; err != nil {
-					return
-				}
-				for _, f := range b.postCreate {
-					if err = f(db, obj, ctx); err != nil {
-						break
-					}
-				}
+		var (
+			db = b.db.Session(&gorm.Session{})
+			do func(state *CallbackState) error
+		)
+		if b.creator == nil {
+			db = b.Prepare(Create, obj, "", nil, ctx)
+			do = func(state *CallbackState) error {
+				return state.DB.Create(state.Obj).Error
 			}
-			return
+		} else {
+			do = func(state *CallbackState) error {
+				return b.creator(state.DB, state.Obj, state.Ctx)
+			}
+		}
+		return WithCallbacks(b.preCreate, b.postCreate, b.db, db, obj, ctx, do)
+	})
+}
+
+func (b *DataOperatorBuilder) Update(obj interface{}, id string, ctx *web.EventContext) (err error) {
+	return b.tx(func(b *DataOperatorBuilder) (err error) {
+		var (
+			db = b.db.Session(&gorm.Session{})
+			do func(state *CallbackState) error
+		)
+
+		if b.updator == nil {
+			db = b.primarySluggerWhere(b.Prepare(Update, obj, id, nil, ctx), obj, id)
+			do = func(state *CallbackState) error {
+				return state.DB.Save(state.Obj).Error
+			}
+		} else {
+			do = func(state *CallbackState) error {
+				return b.updator(state.DB, state.Obj, id, state.Ctx)
+			}
 		}
 
-		if b.updator != nil {
-			return b.updator(b.db, obj, id, ctx)
-		}
-		err = b.primarySluggerWhere(b.Prepare(Update, obj, id, nil, ctx), obj, id).Save(obj).Error
-		return
+		return WithCallbacks(b.preUpdate, b.postUpdate, b.db, db, obj, ctx, do)
 	})
 }
 
@@ -301,4 +350,71 @@ func (b *DataOperatorBuilder) Delete(obj interface{}, id string, ctx *web.EventC
 		err = b.primarySluggerWhere(db, obj, id).Delete(obj).Error
 		return
 	})
+}
+
+type CallbackState struct {
+	SharedDB,
+	DB *gorm.DB
+	Obj   interface{}
+	Ctx   *web.EventContext
+	data  map[any]any
+	dones []func() error
+}
+
+func (s *CallbackState) Done(f func() error) *CallbackState {
+	s.dones = append(s.dones, f)
+	return s
+}
+
+func (s *CallbackState) Set(key any, value any) *CallbackState {
+	s.data[key] = value
+	return s
+}
+
+func (s *CallbackState) Get(key any) (v any, ok bool) {
+	v, ok = s.data[key]
+	return
+}
+
+func WithCallbacks(pre, post []Callback, sharedDB, db *gorm.DB, obj any, ctx *web.EventContext, do func(state *CallbackState) error) (err error) {
+	var (
+		state = &CallbackState{
+			SharedDB: sharedDB,
+			DB:       db,
+			Obj:      obj,
+			Ctx:      ctx,
+			data:     make(map[any]any),
+		}
+	)
+
+	defer func() {
+		var err2 error
+		for _, done := range state.dones {
+			if err2 = done(); err2 != nil {
+				if err == nil {
+					err = err2
+				}
+				break
+			}
+		}
+	}()
+
+	for _, f := range pre {
+		err = f(state)
+		if err != nil {
+			return
+		}
+	}
+
+	if err = do(state); err != nil {
+		return
+	}
+
+	for _, f := range post {
+		err = f(state)
+		if err != nil {
+			return
+		}
+	}
+	return
 }
